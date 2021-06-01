d07a185cd2bfccfd0f9ed669d602f9c4d07872c5
RongshuoLi <66874120+RongshuoLi@users.noreply.github.com>
2021-05-31 23:58:54 -0300

    Create csv.py

---
+++ csv.py
@@ -0,0 +1,24 @@
+
+fileobj = open("data.csv","r") # r,w or a are the options
+delfirst = fileobj.readline()
+datalist = fileobj.readlines()
+fileobj.close()
+#print(datalist)
+
+conflist = []
+for country in datalist:
+    #print(country)
+    templist = country.split(",")
+    pname = templist[2]
+    cname = templist[3]
+    lat = templist[5]
+    lon = templist[6]
+    conf = int(templist[7])
+    #print(pname + cname + lat + lon + conf)
+    conflist.append({"pname":pname,"cname":cname,"lat":lat,"lon":lon,"conf":conf})
+
+#print(conflist)
+conflist.sort(key=lambda s: s['conf'], reverse=True)
+fileout = open("data.js","w")
+fileout.write("data = " + str(conflist))
+fileout.close()
