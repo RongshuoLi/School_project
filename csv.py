a68e91696eabd407801e57b0317fcd12852ed48d
RongshuoLi <66874120+RongshuoLi@users.noreply.github.com>
2021-06-03 10:51:35 -0300

    Update csv.py

--- csv.py
+++ csv.py
@@ -1,5 +1,5 @@
 
-fileobj = open("data.csv","r") # r,w or a are the options
+fileobj = open("covid_data.csv","r") # r,w or a are the options
 delfirst = fileobj.readline()
 datalist = fileobj.readlines()
 fileobj.close()
@@ -17,6 +17,6 @@ for country in datalist:
 
 #print(conflist)
 conflist.sort(key=lambda s: s['conf'], reverse=True)
-fileout = open("data.js","w")
+fileout = open("covid_data.js","w")
 fileout.write("data = " + str(conflist))
 fileout.close()
